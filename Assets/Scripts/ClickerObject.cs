d9429a819cae9cc9c12dbe0b4b98b1fa614fb364
maximpogodin <maxim.pogodin2011@yandex.ru>
2022-05-24 16:45:40 +0500

    Update Scene

--- Assets/Scripts/ClickerObject.cs
+++ Assets/Scripts/ClickerObject.cs
@@ -8,20 +8,23 @@ public class ClickerObject : MonoBehaviour, IPointerClickHandler
     private GameObject particlePrefab;
     public float particleDropForce;
     public float clickPower = 1f;
+    public Vector3 particlePosition; 
+    public float Z;
 
     private void Awake() 
     {
         clickProgression = GameObject.FindObjectOfType<ClickProgression>();
         particlePrefab = Resources.Load<GameObject>("Prefabs/particlePrefab");
-
-        if (GetComponent<BoxCollider>() == null)
-            this.AddComponent<BoxCollider>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        particlePosition = Input.mousePosition;
+        particlePosition.z = Z;
+        particlePosition = Camera.main.ScreenToWorldPoint(particlePosition);
+
         Vector3 randomPosition = new Vector3(Random.RandomRange(-10f, 10f), Random.RandomRange(5f, 10f), Random.RandomRange(-10f, 10f));
-        var particleClone = Instantiate(particlePrefab, eventData.pointerCurrentRaycast.worldPosition, Quaternion.identity);
+        var particleClone = Instantiate(particlePrefab, particlePosition, Quaternion.identity);
         particleClone.GetComponent<Rigidbody>().AddForce(randomPosition * particleDropForce);
 
         Destroy(particleClone, 1f);
